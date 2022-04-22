963d2fcab878e95ed306aa491ca69772f49f14b6
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-04-22 11:08:01 +0700

    Create content from home (left-side)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+  <b-container fluid class="bv-example-row homeContent">
+    <b-row>
+      <b-col style="border: 2px solid black">
+        <div class="contentLeft p-4 m-4">
+          <h1>LOREM IPSUM<br />DOLOR SIT<br />AMET.</h1>
+        </div>
+        <div class="buttonContainer buttonContainer1 p-3 mb-4">
+          <div class="buttonContainer buttonContainer2 p-3">
+            <b-button variant="dark" size="lg" class="homeButton">LOREM</b-button>
+          </div>
+        </div>
+      </b-col>
+      <b-col style="border: 2px solid black">
+        <h1>LOREM</h1>
+      </b-col>
+    </b-row>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+    <!--
+  <b-row>
+    <b-col>1 of 3</b-col>
+    <b-col>2 of 3</b-col>
+    <b-col>3 of 3</b-col>
+  </b-row>
+  -->
+  </b-container>
+</template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1 {
+  font-size: 2.5em;
+  font-weight: bold;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.homeContent {
+  border-top: none;
+  border-bottom: 2px solid black;
+  border-left: 2px solid black;
+  border-right: 2px solid black;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.contentLeft {
+  text-align: left;
 }
-a {
-  color: #42b983;
+.buttonContainer {
+  border: 2px solid black;
+  box-sizing: border-box;
+}
+.buttonContainer1:hover {
+  transition: 0.5s;
+  border-top: none;
+  border-bottom: none;
+  transform: scale(0.9);
+}
+.buttonContainer2:hover {
+  transition: 0.5s;
+  border-left: none;
+  border-right: none;
+  transform: scale(1.1) !important;
+}
+.homeButton {
+  font-weight: bold;
+  border: 2px solid black;
+  width: 98%;
+}
+.homeButton:hover {
+  transition: 0.5s;
+  background-color: #fff;
+  color: #000;
+  font-weight: bold;
+  border: 2px solid #000;
 }
 </style>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
