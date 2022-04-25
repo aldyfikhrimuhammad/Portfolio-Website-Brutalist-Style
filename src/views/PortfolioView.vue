2d91d0d71fc26fea6a8da73f7decdfe84ae60de6
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-04-25 10:45:53 +0700

    Add Favicon & Layouting Portfolio section (Unfinished)

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -7,13 +7,40 @@
       <b-container fluid class="bv-example-row" style="border: 2px solid black">
         <b-row>
           <b-col cols="8" style="border: 2px solid black">
-            <div class="m-2 p-2" style="border: 2px solid black;"></div>
+            <div class="m-2 p-2" style="border: 2px solid black; text-align: left">
+              <div class="title" style="border-bottom: 2px solid black">
+                <h1>Foccu Indonesia</h1>
+                <h5>lorem ipsum dolor sit amet</h5>
+              </div>
+              <div class="description mt-2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,</p>
+              </div>
+            </div>
           </b-col>
-          <b-col cols="4" style="border: 2px solid black; background-color: #000;">
-            <div class="m-2 p-2" style="background-color: #fff;"></div>
+          <b-col cols="4" style="border: 2px solid black; background-color: #000">
+            <div class="m-2" style="background-color: #fff">
+              <img src="@/assets/aboutPictures.png" alt="Profile-Picture" width="100%" height="100%" />
+            </div>
           </b-col>
         </b-row>
-
+        <b-row>
+          <b-col cols="4" style="border: 2px solid black; background-color: #000">
+            <div class="m-2" style="background-color: #fff">
+              <img src="@/assets/aboutPictures.png" alt="Profile-Picture" width="100%" height="100%" />
+            </div>
+          </b-col>
+          <b-col cols="8" style="border: 2px solid black">
+            <div class="m-2 p-2" style="border: 2px solid black; text-align: left">
+              <div class="title" style="border-bottom: 2px solid black">
+                <h1>Foccu Indonesia</h1>
+                <h5>lorem ipsum dolor sit amet</h5>
+              </div>
+              <div class="description mt-2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,</p>
+              </div>
+            </div>
+          </b-col>
+        </b-row>
       </b-container>
     </div>
   </div>
