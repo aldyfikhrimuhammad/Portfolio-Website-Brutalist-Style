08f2f19bcce12c1f607ab96e6a61e874f89c47aa
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-04-25 10:28:53 +0700

    Make Portfolio Section - Layouting

---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="portfolio">
+    <div class="text-center" width="100%" style="border: 2px solid black">
+      <h1>Portfolio</h1>
+    </div>
+    <div>
+      <b-container fluid class="bv-example-row" style="border: 2px solid black">
+        <b-row>
+          <b-col cols="8" style="border: 2px solid black">
+            <div class="m-2 p-2" style="border: 2px solid black;"></div>
+          </b-col>
+          <b-col cols="4" style="border: 2px solid black; background-color: #000;">
+            <div class="m-2 p-2" style="background-color: #fff;"></div>
+          </b-col>
+        </b-row>
+
+      </b-container>
+    </div>
+  </div>
+</template>
